105926a8dd908d398ebdea79f3bdb2b417eca472
BS <bohdan.strilets@gmail.com>
2026-03-18 20:50:20 +0100

    feat(ui): add list with habits

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,31 @@
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Title } from '../components/Title';
 import { Btn } from '../components/Btn';
+import { HabitCard } from '../components/HabitCard';
+
+const habits = [
+  { id: '1', title: 'Drink water' },
+  { id: '2', title: 'Play game' },
+  { id: '3', title: 'Write clean code' },
+];
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Title title="My Habits" />
-      <Btn title="+ Add Habit" />
-    </View>
+    <>
+      <View>
+        <Title title="My Habits" />
+        <Btn title="+ Add Habit" />
+      </View>
+
+      <View>
+        <FlatList
+          data={habits}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <HabitCard title={item.title} onToggle={() => {}} />
+          )}
+        />
+      </View>
+    </>
   );
 };
